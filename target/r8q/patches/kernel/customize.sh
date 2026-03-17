62a3ff4aa2735687e03c408d0463aa1eed5f498b
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-03-17 10:06:09 +0800

    r8q: Fetch paradigm kernel from repo
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- target/r8q/patches/kernel/customize.sh
+++ target/r8q/patches/kernel/customize.sh
@@ -1,3 +1,14 @@
-LOG "- Replacing kernel"
+KERNEL_REPO="https://github.com/pascua28/kernel_samsung_r8q/releases/latest/download"
 
-cp -fa "$SRC_DIR/prebuilts/kernels/r8q/"* "$WORK_DIR/kernel"
+LOG_STEP_IN "- Downloading Paradigm kernel"
+if [ -f "$WORK_DIR/kernel/boot.img" ]; then
+    rm -f "$WORK_DIR/kernel/boot.img"
+fi
+if [ -f "$WORK_DIR/kernel/dtbo.img" ]; then
+    rm -f "$WORK_DIR/kernel/dtbo.img"
+fi
+
+DOWNLOAD_FILE "$KERNEL_REPO/boot.img" "$WORK_DIR/kernel/boot.img"
+DOWNLOAD_FILE "$KERNEL_REPO/dtbo.img" "$WORK_DIR/kernel/dtbo.img"
+unset KERNEL_REPO
+LOG_STEP_OUT
